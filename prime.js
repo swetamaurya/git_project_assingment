d01b3dd090ee08da0cf2b38abf7a194382d7d01d
vivek singh <viveksingh2472@gmail.com>
2022-11-03 14:05:25 +0530

    code_checked

--- prime.js
+++ prime.js
@@ -1,12 +1,12 @@
 //find prime
 function find_prime(n){
 count=0
-for(let i=0;i<=N;i++){
+for(let i=0;i<=n;i++){
     if(n%i==0){
      count++
 }
    }
-if(count==3){
+if(count==2){
 return true
 }return false
 }
